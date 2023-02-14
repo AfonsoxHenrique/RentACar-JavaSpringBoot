c540a9297a40b95ddfcddf4ad2a792d2595e6d9e
AfonsoxHenrique <afonso1240avd@hotmail.com>
2023-02-14 20:21:10 +1100

    Finishing API

--- src/main/java/com/mycompany/rentacar/API/ReceiptAPI.java
+++ src/main/java/com/mycompany/rentacar/API/ReceiptAPI.java
@@ -4,10 +4,51 @@
  */
 package com.mycompany.rentacar.API;
 
+import com.mycompany.rentacar.DTO.ReceiptDTO;
+import com.mycompany.rentacar.facede.ReceiptFacede;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  *
  * @author afons
  */
 public class ReceiptAPI {
+    @Autowired
+    private ReceiptFacede receipt;
     
+    @PostMapping
+    @ResponseBody
+    public ReceiptDTO create (@RequestBody ReceiptDTO receiptDTO)
+    {
+        return receipt.create(receiptDTO);
+    }
+    
+    @PutMapping("/receiptId")
+    @ResponseBody
+    public ReceiptDTO update (@PathVariable("receiptId") Long vehicleId, @RequestBody ReceiptDTO receiptDTO)
+    {
+        return receipt.update(vehicleId, receiptDTO);
+    }
+    
+    @GetMapping
+    @ResponseBody
+    public List<ReceiptDTO> getAll()
+    {
+        return receipt.getAll();
+    }
+    
+    @DeleteMapping("/receiptId")
+    @ResponseBody
+    public String delete (@PathVariable("receiptId") Long receiptId)
+    {
+        return receipt.delete(receiptId);
+    }
 }
